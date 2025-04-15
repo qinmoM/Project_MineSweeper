380a9c32a797d776468c86f016c2ed7675ba37fc
qinmoM <ma200511@qq.com>
2025-04-15 08:56:31 +0800

    Finish display

--- 11-29.cpp
+++ 11-29.cpp
@@ -2,28 +2,33 @@
 
 #include"11-29.h"
 
-void init_board(char arr[ROWS][COLS], int rows, int cols, char set)
+MOUSEMSG msg;
+
+bool type = 1;// 判断是否在主界面
+const wchar_t* symbol[8] = { L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8" };// 用于提示数字
+
+void init_board(char arr[ROWS][COLS], char set)
 {
 	int i = 0, j = 0;
-	for (i = 0; i < rows; i++)
+	for (i = 0; i < ROWS; i++)
 	{
-		for (j = 0; j < cols; j++)
+		for (j = 0; j < COLS; j++)
 		{
 			arr[i][j] = set;
 		}
 	}
 }
 
-void random(char arr[ROWS][COLS], int rows, int cols, int mine)
+void random(char arr1[ROWS][COLS], int mine)
 {
 	int m = 0, j = 0, i = 0;
 	for (m = 0; m < mine; m++)
 	{
 		i = rand() % ROW + 1;
 		j = rand() % COL + 1;
-		if ('1' != arr[i][j])
+		if ('1' != arr1[i][j])
 		{
-			arr[i][j] = '1';
+			arr1[i][j] = '1';
 		}
 		else
 		{
@@ -32,95 +37,152 @@ void random(char arr[ROWS][COLS], int rows, int cols, int mine)
 	}
 }
 
-void show(char arr[ROWS][COLS], int rows, int cols)
+void Menu()// 绘制主界面
 {
-	int i = 0, j = 0;
-	printf("\n  ");
-	for (i = 1; i < cols - 1; i++)
-	{
-		printf("^%d", i);
-	}
-	printf("\n");
-	for (i = 1; i < rows - 1; i++)
-	{
-		printf("%2d", i);
-		for (j = 1; j < cols - 1; j++)
-		{
-			printf("%2c", arr[i][j]);
-		}
-		printf("\n");
-	}
+	BeginBatchDraw();//防止闪烁
+	cleardevice();// 刷新背景
+	rectangle(SIZE * COL / 2 - 100, SIZE * ROW / 2 - 110,
+		SIZE * COL / 2 + 100, SIZE * ROW / 2 - 30);// 绘制选项卡
+	rectangle(SIZE * COL / 2 - 100, SIZE * ROW / 2 + 30,
+		SIZE * COL / 2 + 100, SIZE * ROW / 2 + 110);// 绘制选项卡
+	outtextxy((SIZE * COL - textwidth(L"开始游戏")) / 2,
+		SIZE * ROW / 2 - 110 + (80 - textheight(L"开始游戏")) / 2, L"开始游戏");// 开始游戏
+	outtextxy((SIZE * COL - textwidth(L"退出游戏")) / 2,
+		SIZE * ROW / 2 + 30 + (80 - textheight(L"退出游戏")) / 2, L"退出游戏");// 退出游戏
+	EndBatchDraw();// 关闭双缓冲绘图
 }
 
-char calculate(char arr[ROWS][COLS], int x, int y)
+void BoardDraw(char arr1[ROWS][COLS], char arr2[ROWS][COLS])// 绘制游戏内容
 {
-	return arr[x - 1][y - 1] + arr[x][y - 1] + arr[x + 1][y - 1] + arr[x - 1][y] + arr[x + 1][y]
-			+ arr[x - 1][y + 1] + arr[x][y + 1] + arr[x + 1][y + 1] - 7 * '0';
+	BeginBatchDraw();//防止闪烁
+	for (int i = 1; i < ROWS - 1; i++)
+	{
+		for (int j = 1; j < COLS - 1; j++)
+		{
+			if ('*' == arr2[i][j])// 未翻起
+			{
+				setfillcolor(COLOR);// 暗色
+				fillrectangle(SIZE * (i - 1), SIZE * (j - 1), SIZE * i, SIZE * j);
+			}
+			else if ('0' == arr2[i][j])// 翻起但是为0
+			{
+				setfillcolor(WHITE);// 白色
+				fillrectangle(SIZE * (i - 1), SIZE * (j - 1), SIZE * i, SIZE * j);
+			}
+			else if ('1' == arr1[i][j])// 雷
+			{
+				setfillcolor(RED);// 红色
+				fillrectangle(SIZE * (i - 1), SIZE * (j - 1), SIZE * i, SIZE * j);
+			}
+			else// 其他数字
+			{
+				int number = arr2[i][j] - '0' - 1;
+				setfillcolor(WHITE);// 白色
+				fillrectangle(SIZE * (i - 1), SIZE * (j - 1), SIZE * i, SIZE * j);
+				outtextxy((SIZE * (2 * i - 1) - textwidth(symbol[number])) / 2,
+					(SIZE * (2 * j - 1) - textheight(symbol[number])) / 2,
+					symbol[number]);
+			}
+		}
+	}
+	EndBatchDraw();// 关闭双缓冲绘图
+}
+
+int Judge(char arr2[ROWS][COLS])// 遍历arr2中的未翻起元素数量
+{
+	int num = 0;
+	for (int i = 1; i < ROWS - 1; i++)
+	{
+		for (int j = 1; j < COLS - 1; j++)
+		{
+			if ('*' == arr2[i][j])
+			{
+				num++;
+			}
+		}
+	}
+	return num;
+}
+
+void Control(char arr1[ROWS][COLS], char arr2[ROWS][COLS], short m, short n)// 局内控制
+{
+	short x = m / SIZE + 1, y = n / SIZE + 1;
+	if (7 == x)
+	{
+		x = 6;
+	}
+	if (7 == y)
+	{
+		y = 6;
+	}
+	arr2[x][y] = arr1[x - 1][y - 1] + arr1[x][y - 1] + arr1[x + 1][y - 1] + arr1[x - 1][y]
+		+ arr1[x + 1][y] + arr1[x - 1][y + 1] + arr1[x][y + 1] + arr1[x + 1][y + 1] - 7 * '0';
+		BoardDraw(arr1, arr2);
+		if ('1' == arr1[x][y])// 判断是否踩雷
+		{
+			Sleep(1500);
+			FlushMouseMsgBuffer();// 清空鼠标信息
+			type = !type;// 切换鼠标控制逻辑
+			Menu();// 绘制主界面
+			outtextxy((getwidth() - textwidth(L"您已阵亡")) / 2,
+				SIZE * ROW / 2 - 110 - textheight(L"您已阵亡") - 20, L"您已阵亡");// 绘制输
+		}
+		if (MINE == Judge(arr2))// 判断是否赢
+		{
+			type = !type;// 切换鼠标控制逻辑
+			Menu();// 绘制主界面
+			outtextxy((getwidth() - textwidth(L"恭喜胜利")) / 2,
+				SIZE * ROW / 2 - 110 - textheight(L"恭喜胜利") - 20, L"恭喜胜利");// 绘制赢
+		}
+}
+
+void GameControl(char arr1[ROWS][COLS], char arr2[ROWS][COLS])//鼠标信息控制
+{
+	if (MouseHit())
+	{
+		msg = GetMouseMsg();
+		if (WM_LBUTTONUP == msg.uMsg)
+		{
+			if (type)// 是否为主菜单
+			{
+				if (msg.x > SIZE * COL / 2 - 100 && msg.x < SIZE * COL / 2 + 100
+					&& msg.y > SIZE * ROW / 2 - 110 && msg.y < SIZE * ROW / 2 - 30)// 是否选中新游戏
+				{
+					type = !type;// 切换鼠标控制逻辑
+					init_board(arr1, '0');
+					random(arr1, MINE);
+					init_board(arr2, '*');
+					BoardDraw(arr1, arr2);// 绘制游戏内容
+				}
+				else if (msg.x > SIZE * COL / 2 - 100 && msg.x < SIZE * COL / 2 + 100
+					&& msg.y > SIZE * ROW / 2 + 30 && msg.y < SIZE * ROW / 2 + 110)// 是否选中退出
+				{
+					exit(0);
+				}
+			}
+			else// 局内
+			{
+				Control(arr1, arr2, msg.x, msg.y);// 局内控制
+			}
+		}
+	}
 }
 
 int main()
 {
+	initgraph(SIZE * COL, SIZE * ROW);
+	setbkcolor(COLOR);// 设置背景色
+	setlinecolor(BLACK);// 设置线条颜色
+	settextcolor(BLACK);// 设置字体颜色
+	settextstyle(35, 0, L"宋体");// 设置字体样式
 	int i = 0, j = 0, m = 0, input = 0, round = 0, u = 0;
 	char arr1[ROWS][COLS] = { 0 };
 	char arr2[ROWS][COLS] = { 0 };
-	printf("**************欢迎来到扫雷游戏!**************\n");
-	printf("*********************************************\n");
-	printf("*****************1.开始游戏******************\n");
-	printf("*****************0.退出游戏******************\n");
-	printf("***********请输入数字进行下一步操作**********\n");
 	srand((unsigned int)time(NULL));
+	Menu();// 绘制主界面
 	do
 	{
-		if (0 < m)
-		{
-			printf("\n*****************1.再来一局******************\n");
-			printf("*****************0.退出游戏******************\n");
-		}
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 0:
-			return 0;
-		case 1:
-			u = 0;
-			init_board(arr1, ROWS, COLS, '0');
-			random(arr1, ROWS, COLS, MINE);
-			init_board(arr2, ROWS, COLS, '*');
-			for (round = 0; round < ROW * COL - MINE; round++)
-			{
-				show(arr2, ROWS, COLS);
-				printf("\n请输入坐标(格式为：坐标>空格>含^坐标) :>\n");
-				scanf("%d %d", &i, &j);
-				if ('*' != arr2[i][j] || i < 1 || j < 1 || i > ROW || j > COL)
-				{
-					printf("坐标非法,请重新输入!");
-					round--;
-				}
-				else if ('1' == arr1[i][j])
-				{
-					u++;
-					break;
-				}
-				else
-				{
-					arr2[i][j] = calculate(arr1, i, j);
-				}
-			}
-			if (0 == u)
-			{
-				printf("恭喜您，游戏胜利!");
-				show(arr1, ROWS, COLS);
-			}
-			else
-			{
-				printf("您已死亡，游戏结束");
-				show(arr1, ROWS, COLS);
-			}
-			break;
-		default:
-			printf("请输入正确数字\n");
-		}
-		m++;
+		GameControl(arr1, arr2);//鼠标信息控制
 	} while (1);
 	return 0;
 }
--- 11-29.h
+++ 11-29.h
@@ -3,14 +3,20 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+#include <graphics.h>
 
 #define ROW 6//行数(可改)
 #define COL 6//列数(可改)
 #define ROWS ROW + 2
 #define COLS COL + 2
 #define MINE 6//雷数(可改)
+#define SIZE 100// 格子大小
+#define COLOR RGB(210, 210, 210)// 背景颜色
 
-void init_board1(char arr[ROWS][COLS], int rows, int cols, char set);//初始化棋盘为'set'字符
-void random(char arr1[ROWS][COLS], int rows, int cols, int mine);//对棋盘随机下mine个雷
-void show(char arr[ROWS][COLS], int rows, int cols);//打印数组棋盘
-char calculate(char arr[ROWS][COLS], int x, int y);//返回x y坐标周围雷的个数
+void init_board(char arr[ROWS][COLS], char set);//初始化棋盘为'set'字符
+void random(char arr1[ROWS][COLS], int mine);//对棋盘随机下mine个雷
+void Menu();// 绘制主界面
+void BoardDraw(char arr1[ROWS][COLS], char arr2[ROWS][COLS]);// 绘制游戏内容
+int Judge(char arr2[ROWS][COLS]);// 遍历arr2中的未翻起元素数量
+void Control(char arr1[ROWS][COLS], char arr2[ROWS][COLS], short m, short n);// 局内控制
+void GameControl(char arr1[ROWS][COLS], char arr2[ROWS][COLS]);//鼠标信息控制
