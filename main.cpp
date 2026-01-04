50fccd45c6bd9f54766cb7e652383ee22a5b5181
qinmoM <ma200511@qq.com>
2026-01-05 01:01:46 +0800

    main : test code of renderer.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
-#include "raylib.h"
+#include "RendererRaylib.h"
 
 int main()
 {
-    InitWindow(200, 200, "test");
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
+    RendererBase& renderer = RendererRaylib::getInstance();
+    renderer.windowInit(600, 700, "test");
+    while (renderer.windowExists())
     {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        EndDrawing();
+        renderer.drawingBegin();
+
+        renderer.clearBackground(Base::ColorBase{255, 255, 255, 0});
+
+        renderer.drawCircle(200, 200, 10, 20, Base::ColorBase{255, 0, 0, 255});
+        renderer.drawCircleFill(300, 300, 10, Base::ColorBase{0, 255, 0, 255});
+        renderer.drawLine(0, 0, 600, 700, 2, Base::ColorBase{0, 0, 255, 255});
+        renderer.drawLineAngle(300, 300, 10, 100, 2, Base::ColorBase{0, 0, 0, 255});
+        renderer.drawRectangle(100, 100, 200, 200, 4, Base::ColorBase{0, 255, 255, 255});
+        renderer.drawRectangleFill(400, 300, 100, 100, Base::ColorBase{255, 0, 255, 255});
+        renderer.drawRectangleRound(400, 0, 200, 200, 0.6f, 10, 4, Base::ColorBase{111, 111, 200, 255});
+        renderer.drawRectangleRoundFill(0, 400, 100, 200, 0.3f, 2, Base::ColorBase{0, 0, 0, 255});
+
+        renderer.drawingEnd();
     }
-    CloseWindow();
+    renderer.windowClose();
     return 0;
 }
