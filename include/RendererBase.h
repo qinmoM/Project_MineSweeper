e90e071179d6d5c03478ce21d2995ccea2749db7
qinmoM <ma200511@qq.com>
2026-01-05 00:50:18 +0800

    RendererBase : adjust API.

--- include/RendererBase.h
+++ include/RendererBase.h
@@ -11,18 +11,17 @@ public:
     virtual bool windowExists() = 0;
 
     virtual void drawingBegin() = 0;
-    virtual void drawingFlush() = 0;
     virtual void drawingEnd() = 0;
 
     virtual void clearBackground(const Base::ColorBase& color) = 0;
 
     virtual void drawLine(int startX, int startY, int endX, int endY, int thick, const Base::ColorBase& color) = 0;
+    virtual void drawLineAngle(int startX, int startY, float angle, float length, int thick, const Base::ColorBase& color) = 0;
     virtual void drawRectangle(int posX, int posY, int width, int height, int thick, const Base::ColorBase& color) = 0;
-    virtual void drawRectangleRound(int posX, int posY, int roundness, int segments, int thick, const Base::ColorBase& color) = 0;
-    virtual void drawCircle(int posX, int posY, float radius, int thick, const Base::ColorBase& color) = 0;
+    virtual void drawRectangleRound(int posX, int posY, int width, int height, float roundness, int segments, int thick, const Base::ColorBase& color) = 0;
+    virtual void drawCircle(int posX, int posY, float innerRadius, float outerRadius, const Base::ColorBase& color) = 0;
 
-    virtual void drawLineFill(int startX, int startY, int endX, int endY, const Base::ColorBase& color) = 0;
     virtual void drawRectangleFill(int posX, int posY, int width, int height, const Base::ColorBase& color) = 0;
-    virtual void drawRectangleRoundFill(int posX, int posY, int roundness, int segments, const Base::ColorBase& color) = 0;
+    virtual void drawRectangleRoundFill(int posX, int posY, int width, int height, float roundness, int segments, const Base::ColorBase& color) = 0;
     virtual void drawCircleFill(int posX, int posY, float radius, const Base::ColorBase& color) = 0;
 };
