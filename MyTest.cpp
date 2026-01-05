9586b5dfad857529b5113bfe4a98415c9ced397f
qinmoM <ma200511@qq.com>
2026-01-05 13:51:56 +0800

    refactor: Separate main.cpp and MyTest.cpp. Default MyTest.cpp.

---
+++ MyTest.cpp
@@ -0,0 +1,20 @@
+#include "RendererRaylib.h"
+
+int main()
+{
+    RendererBase& renderer = RendererRaylib::getInstance();
+    renderer.windowInit(600, 700, "test");
+
+    while (renderer.windowExists())
+    {
+        renderer.drawingBegin();
+        renderer.clearBackground(Base::ColorBase{255, 255, 255, 0});
+
+        ;
+
+        renderer.drawingEnd();
+    }
+
+    renderer.windowClose();
+    return 0;
+}
