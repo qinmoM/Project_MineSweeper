f2a35e73e0a66fa1b9eff2bbadda680b675941f5
qinmoM <ma200511@qq.com>
2026-01-06 00:22:17 +0800

    fear(GameStateBase class): State Pattern base class of game.

---
+++ include/GameStateBase.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class GameStateBase
+{
+public:
+    virtual void enter() = 0;
+    virtual void exit() = 0;
+
+    virtual void update(float delta) = 0;
+    virtual void render() = 0;
+public:
+    ~GameStateBase() = default;
+};
