e9e908e69f72936e69ca82c09f453474486e6573
qinmoM <ma200511@qq.com>
2026-01-06 12:07:55 +0800

    refactor: refactor project structure and CMake config.

--- MyTest.cpp
+++ MyTest.cpp
@@ -1,23 +1,34 @@
-#include "RendererRaylib.h"
+#include "Game.h"
 
 int main()
 {
-    RendererBase& renderer = RendererRaylib::getInstance();
-    renderer.windowInit(600, 700, "test");
-
-    renderer.FPSset(60);
-
-    while (renderer.windowExists())
-    {
-        renderer.drawingBegin();
-        renderer.clearBackground(Base::ColorBase{255, 255, 255, 0});
-
-        ;
-
-        renderer.drawingEnd();
-        renderer.FPSDelay();
-    }
-
-    renderer.windowClose();
+    Game game;
+    game.run();
     return 0;
 }
+
+
+
+// #include "RendererRaylib.h"
+
+// int main()
+// {
+//     RendererBase& renderer = RendererRaylib::getInstance();
+//     renderer.windowInit(600, 700, "test");
+
+//     renderer.FPSset(60);
+
+//     while (renderer.windowExists())
+//     {
+//         renderer.drawingBegin();
+//         renderer.clearBackground(Base::ColorBase{255, 255, 255, 0});
+
+//         ;
+
+//         renderer.drawingEnd();
+//         renderer.FPSDelay();
+//     }
+
+//     renderer.windowClose();
+//     return 0;
+// }
