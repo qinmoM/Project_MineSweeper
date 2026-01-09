23a44c5807be15a14b6cc8ce89b3aa0c353e12e4
qinmoM <ma200511@qq.com>
2026-01-09 11:45:13 +0800

    feat(MouseBase class): add Mouse-related API.

---
+++ include/handle/mouse/MouseBase.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "BasePoint.h"
+#include "BaseMouseButton.h"
+#include "BaseMouseCursor.h"
+
+class MouseBase
+{
+public:
+    // sustained check
+    virtual bool buttonDown(Base::MouseButton button) = 0;
+    // single check
+    virtual bool buttonPressed(Base::MouseButton button) = 0;
+    virtual bool buttonReleased(Base::MouseButton button) = 0;
+
+    // position
+    virtual Base::Point getPosition() = 0;
+    virtual void setPosition(const Base::Point& position) = 0;
+
+    // wheel
+    virtual float getWheelMove() = 0;
+
+    // cursor
+    virtual void setCursor(Base::MouseCursor cursor) = 0;
+
+public:
+    virtual ~MouseBase() = default;
+};
