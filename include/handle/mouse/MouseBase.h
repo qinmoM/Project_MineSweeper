67656ba74a7641fb91965f1bd0d215ecee28ddba
qinmoM <ma200511@qq.com>
2026-01-10 18:04:36 +0800

    refactor(MouseBase class)

--- include/handle/mouse/MouseBase.h
+++ include/handle/mouse/MouseBase.h
@@ -8,18 +8,18 @@ class MouseBase
 {
 public:
     // sustained check
-    virtual bool buttonDown(Base::MouseButton button) = 0;
+    virtual bool buttonDown(Base::MouseButton button) const = 0;
     // single check
-    virtual bool buttonPressed(Base::MouseButton button) = 0;
-    virtual bool buttonReleased(Base::MouseButton button) = 0;
+    virtual bool buttonPressed(Base::MouseButton button) const = 0;
+    virtual bool buttonReleased(Base::MouseButton button) const = 0;
 
     // position
-    virtual Base::Point getPosition() = 0;
-    virtual bool inWindow() = 0;
+    virtual Base::Point getPosition() const = 0;
+    virtual bool inWindow() const = 0;
     virtual void setPosition(const Base::Point& position) = 0;
 
     // wheel
-    virtual float getWheelMove() = 0;
+    virtual float getWheelMove() const = 0;
 
     // cursor
     virtual void setCursor(Base::MouseCursor cursor) = 0;
