abd56e3e892bb2200dd51cfb92911b3437d66ffe
qinmoM <ma200511@qq.com>
2026-01-18 23:55:23 +0800

    feat(HandleInputSemantic class): add clicked, doubleClicked, longPressed function.

---
+++ include/handle/handle/HandleInputSemantic.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "HandleInputBase.h"
+#include "ToolClock.h"
+#include <memory>
+#include <unordered_map>
+
+struct ClickState
+{
+    Base::Point position_{0.0f, 0.0f};
+    float time_ = 0.0f;
+    float lastClickTime_ = 0.0f;
+    bool thisFrame_ = false;
+    bool thisDoubleFrame_ = false;
+    bool isAfterClick_ = false;
+};
+
+class HandleInputSemantic
+{
+public:
+
+    //              semantic
+
+    bool mouseClicked(Base::MouseButton button) const;
+    bool mouseDoubleClicked(Base::MouseButton button) const;
+    bool mouseLongPressed(Base::MouseButton button) const;
+
+    //              base
+
+    void update(float delta);
+    // keyboard
+    bool keyDown(Base::Key key) const;
+    bool keyPressed(Base::Key key) const;
+    bool keyReleased(Base::Key key) const;
+    // mouse
+    bool mouseDown(Base::MouseButton button) const;
+    bool mousePressed(Base::MouseButton button) const;
+    bool mouseReleased(Base::MouseButton button) const;
+    Base::Point mousePosition() const;
+    bool mouseInWindow() const;
+    void mouseSetPosition(Base::Point position);
+    float mouseWheelMove() const;
+    void mouseSetCursor(Base::MouseCursor cursor);
+public:
+    HandleInputSemantic(std::shared_ptr<HandleInputBase> handle);
+protected:
+    std::shared_ptr<HandleInputBase> handle_;
+    std::unordered_map<Base::MouseButton, ClickState> mouseClickStates_;
+    float maxClickTime_ = 0.5f;
+    float maxMoveDistance_ = 5.0f;
+    float maxDoubleClickTime_ = 2.0f;
+    float minLongPressTime_ = 1.0f;
+};
---
+++ src/handle/handle/HandleInputSemantic.cpp
@@ -0,0 +1,131 @@
+#include "HandleInputSemantic.h"
+
+
+HandleInputSemantic::HandleInputSemantic(std::shared_ptr<HandleInputBase> handle) : handle_(handle)
+{
+    mouseClickStates_[Base::MouseButton::Left] = ClickState();
+    mouseClickStates_[Base::MouseButton::Right] = ClickState();
+    mouseClickStates_[Base::MouseButton::Middle] = ClickState();
+    mouseClickStates_[Base::MouseButton::forward] = ClickState();
+    mouseClickStates_[Base::MouseButton::back] = ClickState();
+}
+
+
+bool HandleInputSemantic::mouseClicked(Base::MouseButton button) const
+{
+    return mouseClickStates_.at(button).thisFrame_;
+}
+
+bool HandleInputSemantic::mouseDoubleClicked(Base::MouseButton button) const
+{
+    return mouseClickStates_.at(button).thisDoubleFrame_;
+}
+
+bool HandleInputSemantic::mouseLongPressed(Base::MouseButton button) const
+{
+    return (mouseClickStates_.at(button).time_ >= minLongPressTime_) && (mouseDown(button));
+}
+
+//              base
+void HandleInputSemantic::update(float delta)
+{
+    handle_->update(delta);
+
+    for (auto& [button, state] : mouseClickStates_)
+    {
+        state.thisFrame_ = false;
+        state.thisDoubleFrame_ = false;
+
+        if (mouseDown(button))
+            state.time_ += delta;
+
+        if (mousePressed(button))
+        {
+            state.time_ = 0.0f;
+            state.position_ = mousePosition();
+        }
+
+        if (state.isAfterClick_)
+        {
+            state.lastClickTime_ += delta;
+            if (state.lastClickTime_ >= maxDoubleClickTime_ - maxClickTime_)
+                state.isAfterClick_ = false;
+        }
+
+        if (mouseReleased(button))
+        {
+            Base::Point dPosition = mousePosition() - state.position_;
+            if ((state.time_ <= maxClickTime_) && (dPosition.x * dPosition.x + dPosition.y * dPosition.y <= maxMoveDistance_ * maxMoveDistance_))
+            {
+                state.thisFrame_ = true;
+                if (state.isAfterClick_)
+                {
+                    state.thisDoubleFrame_ = true;
+                    state.isAfterClick_ = false;
+                }
+                else
+                {
+                    state.lastClickTime_ = 0.0f;
+                    state.isAfterClick_ = true;
+                }
+            }
+        }
+    }
+}
+
+// keyboard
+bool HandleInputSemantic::keyDown(Base::Key key) const
+{
+    return handle_->keyDown(key);
+}
+
+bool HandleInputSemantic::keyPressed(Base::Key key) const
+{
+    return handle_->keyPressed(key);
+}
+
+bool HandleInputSemantic::keyReleased(Base::Key key) const
+{
+    return handle_->keyReleased(key);
+}
+
+// mouse
+bool HandleInputSemantic::mouseDown(Base::MouseButton button) const
+{
+    return handle_->mouseDown(button);
+}
+
+bool HandleInputSemantic::mousePressed(Base::MouseButton button) const
+{
+    return handle_->mousePressed(button);
+}
+
+bool HandleInputSemantic::mouseReleased(Base::MouseButton button) const
+{
+    return handle_->mouseReleased(button);
+}
+
+Base::Point HandleInputSemantic::mousePosition() const
+{
+    return handle_->mousePosition();
+}
+
+bool HandleInputSemantic::mouseInWindow() const
+{
+    return handle_->mouseInWindow();
+}
+
+void HandleInputSemantic::mouseSetPosition(Base::Point position)
+{
+    return handle_->mouseSetPosition(position);
+}
+
+float HandleInputSemantic::mouseWheelMove() const
+{
+    return handle_->mouseWheelMove();
+}
+
+void HandleInputSemantic::mouseSetCursor(Base::MouseCursor cursor)
+{
+    return handle_->mouseSetCursor(cursor);
+}
