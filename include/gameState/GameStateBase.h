e6a0d0e2a15cc6ade26032269a81cd72b6bf1e06
qinmoM <ma200511@qq.com>
2026-01-20 23:07:40 +0800

    fix(GameStateBase): GameStateManager-related.

--- include/gameState/GameStateBase.h
+++ include/gameState/GameStateBase.h
@@ -4,6 +4,8 @@
 #include "HandleInputSemantic.h"
 #include <memory>
 
+class GameStateManager;
+
 class GameStateBase
 {
 public:
@@ -13,9 +15,14 @@ public:
     virtual void update(float delta) = 0;
     virtual void render() = 0;
 public:
-    GameStateBase(std::shared_ptr<RendererProxy> renderer, std::shared_ptr<HandleInputSemantic> handleInput) : renderer_(renderer), handleInput_(handleInput) { }
+    GameStateBase(std::shared_ptr<RendererProxy> renderer, std::shared_ptr<HandleInputSemantic> handleInput, std::shared_ptr<GameStateManager> gameStateManager)
+        : renderer_(renderer)
+        , handleInput_(handleInput)
+        , stateManager_(gameStateManager)
+    { }
     virtual ~GameStateBase() = default;
 protected:
     std::shared_ptr<RendererProxy> renderer_;
     std::shared_ptr<HandleInputSemantic> handleInput_;
+    std::shared_ptr<GameStateManager> stateManager_;
 };
