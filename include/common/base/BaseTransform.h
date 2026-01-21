33efb171d716b9f9bb2bbce54af7bcd4e4382f26
qinmoM <ma200511@qq.com>
2026-01-21 18:48:14 +0800

    feat(BaseTrasform struct): Add constrctor.

--- include/common/base/BaseTransform.h
+++ include/common/base/BaseTransform.h
@@ -5,9 +5,10 @@
 namespace Base
 {
 
-class Transform
+struct Transform
 {
-public:
+    Transform() : position_(0.0f, 0.0f), scale_(1.0f, 1.0f), origin_(0.0f, 0.0f), rotation_(0) {}
+
     Base::Point position_;
     Base::Point scale_;
     Base::Point origin_;
