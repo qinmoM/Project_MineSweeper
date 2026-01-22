2da5ef29edc9735378ce18a777117dc1dae21bd4
qinmoM <ma200511@qq.com>
2026-01-22 12:29:05 +0800

    feat(Color): Add operator overloading function for '==' and '!='.

--- include/common/base/BaseColor.h
+++ include/common/base/BaseColor.h
@@ -9,6 +9,16 @@ struct Color
     unsigned char g;        // Color green value
     unsigned char b;        // Color blue value
     unsigned char a;        // Color alpha value
+
+    bool operator==(const Color& other) const
+    {
+        return r == other.r && g == other.g && b == other.b && a == other.a;
+    }
+
+    bool operator!=(const Color& other) const
+    {
+        return r != other.r || g != other.g || b != other.b || a != other.a;
+    }
 };
 
 }
