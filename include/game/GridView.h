8d2d7188ce04b233894f169756a93cd04b7ec4ac
qinmoM <ma200511@qq.com>
2026-01-27 18:46:03 +0800

    feat(GridView class): Add view class of MineField.

---
+++ include/game/GridView.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "MineField.h"
+#include <ctime>
+#include <random>
+#include <memory>
+
+class GridView
+{
+public:
+    void regenerateMines(int numMines);
+    void reveal(int row, int col);
+    const Cell& getCell(int row, int col) const;
+    const MineField& getField() const;
+public:
+    GridView(int rows, int cols, int numMines);
+protected:
+    std::unique_ptr<MineField> field_;
+    int revealCount_ = 0;
+    bool gameOver_ = false;
+};
---
+++ src/game/GridView.cpp
@@ -0,0 +1,61 @@
+#include "GridView.h"
+
+GridView::GridView(int rows, int cols, int numMines)
+{
+    if (rows * cols <= numMines)
+        throw std::invalid_argument("Too many mines for grid size. | GridView::GridView");
+
+    field_ = std::make_unique<MineField>(rows, cols);
+    regenerateMines(numMines);
+}
+
+void GridView::regenerateMines(int numMines)
+{
+    field_->reset();
+    for (int i = 0; i < field_->getNumMines(); )
+    {
+        int index = rand() % (field_->getRows() * field_->getCols());
+        int row = index / field_->getCols();
+        int col = index % field_->getCols();
+        if (!field_->getCell(row, col).isMine_)
+        {
+            field_->getCell(row, col).isMine_ = true;
+            ++i;
+        }
+    }
+    field_->flushNearbyNum();
+}
+
+void GridView::reveal(int row, int col)
+{
+    if (row < 0 || row >= field_->getRows() || col < 0 || col >= field_->getCols() || field_->getCell(row, col).isRevealed_ || gameOver_)
+        return;
+
+    if (field_->getCell(row, col).isMine_)
+        gameOver_ = true;
+
+    field_->getCell(row, col).isRevealed_ = true;
+    revealCount_ += 1;
+
+    if (0 == field_->getCell(row, col).numMinesNearby_)
+    {
+        reveal(row - 1, col - 1);
+        reveal(row - 1, col);
+        reveal(row - 1, col + 1);
+        reveal(row, col - 1);
+        reveal(row, col + 1);
+        reveal(row + 1, col - 1);
+        reveal(row + 1, col);
+        reveal(row + 1, col + 1);
+    }
+}
+
+const Cell& GridView::getCell(int row, int col) const
+{
+    return field_->getCell(row, col);
+}
+
+const MineField& GridView::getField() const
+{
+    return *field_;
+}
