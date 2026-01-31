aabf80aa93b3f41d92ef3b5943be38caeaeb0ccc
qinmoM <ma200511@qq.com>
2026-01-31 15:25:14 +0800

    fix(RayliyAudio)

--- include/audio/device/RaylibAudio.h
+++ include/audio/device/RaylibAudio.h
@@ -9,8 +9,29 @@ class RaylibAudio final : public IAudioSystem
 public:
     static RaylibAudio& getInstance();
 public:
-    std::shared_ptr<ISound> loadSound(const std::string& filePath) override;
-    std::shared_ptr<IMusic> loadMusic(const std::string& filePath) override;
+    void update() override;
+
+    void setBGMVolume(float volume) override;
+    void setAmbientVolume(float volume) const override;
+    void setSFXVolume(float volume) override;
+
+    void loadBGM(const std::string& filePath) override;
+    void playBGM() override;
+    void stopBGM() override;
+    void pauseBGM() override;
+    void resumeBGM() override;
+
+    void registerAmbient(const std::string& name, const std::string& filePath) override;
+    void unregisterAmbient(const std::string& name) override;
+    void playAmbient(const std::string& name) override;
+    void stopAmbient(const std::string& name) override;
+    void pauseAmbient(const std::string& name) override;
+    void resumeAmbient(const std::string& name) override;
+
+    void registerSFX(const std::string& name, const std::string& filePath, uint8_t poolSize = 1) override;
+    void unregisterSFX(const std::string& name) override;
+    void playSFX(const std::string& name) override;
+    void stopSFX(const std::string& name) override;
 public:
     RaylibAudio(const RaylibAudio&) = delete;
     RaylibAudio& operator=(const RaylibAudio&) = delete;
