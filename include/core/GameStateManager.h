bf343ced3d7a0b26af5289a0cbff35647309eac1
qinmoM <ma200511@qq.com>
2026-02-03 20:19:49 +0800

    feat(GameStateManager):  Change task queue to task map, and add priority and delay to tasks.

--- include/core/GameStateManager.h
+++ include/core/GameStateManager.h
@@ -4,13 +4,21 @@
 #include <functional>
 #include <stdexcept>
 #include <vector>
-#include <queue>
+#include <map>
 
 class GameStateManager
 {
 public:
     using stateType = std::unique_ptr<GameStateBase>;
     using Creator = std::function<stateType()>;
+public:
+    struct Task
+    {
+        Task(const std::function<void()>& task, float frameDelay) : task_(task), frameDelay_(frameDelay) { }
+
+        std::function<void()> task_;
+        float frameDelay_;
+    };
 public:
     void registerState(std::string stateName, Creator creator);
     void unregisterState(std::string stateName);
@@ -19,14 +27,14 @@ public:
     void popState();
     void clearState();
 
-    void addTask(const std::function<void()>& task);
+    void addTask(const std::function<void()>& task, float priority = 0.5f, float frameDelay = 0.0f);
     void clearTask();
 
     void requestQuit();
     bool shouldQuit() const;
 protected:
     std::vector<stateType> stateStack_;
-    std::queue<std::function<void()>> tasksQueue_;
+    std::multimap<float, Task> tasksQueue_;
     std::unordered_map<std::string, Creator> registerCreators_;
     bool requestQuit_ = false;
     friend class GameManager;
