86490ee4eb4dc514db8957acf305d9a05ec70a68
qinmoM <ma200511@qq.com>
2026-02-04 15:50:21 +0800

    feat(BasePoint): Add operator of greater and less.

--- include/common/base/BasePoint.h
+++ include/common/base/BasePoint.h
@@ -99,4 +99,24 @@ inline bool operator!=(const Point& a, const Point& b)
     return (a.x != b.x) || (a.y != b.y);
 }
 
+inline bool operator<(const Point& a, const Point& b)
+{
+    return (a.x < b.x) && (a.y < b.y);
+}
+
+inline bool operator<=(const Point& a, const Point& b)
+{
+    return (a.x <= b.x) && (a.y <= b.y);
+}
+
+inline bool operator>(const Point& a, const Point& b)
+{
+    return (a.x > b.x) && (a.y > b.y);
+}
+
+inline bool operator>=(const Point& a, const Point& b)
+{
+    return (a.x >= b.x) && (a.y >= b.y);
+}
+
 }
