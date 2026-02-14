5b7b8c6599ab94d35c4b82e9f2bef88f135453ef
qinmoM <ma200511@qq.com>
2026-02-14 23:52:06 +0800

    refactor(GameStateBase): Add name of state.

--- include/gameState/GameStateBase.h
+++ include/gameState/GameStateBase.h
@@ -42,11 +42,13 @@ public:
     virtual void update(float delta) = 0;
     virtual void render() = 0;
 public:
-    GameStateBase(GameStateContext& context)
+    GameStateBase(GameStateContext& context, const std::string& stateName)
         : context_(context)
+        , stateName_(stateName)
     { }
     virtual ~GameStateBase() = default;
 protected:
     GameStateContext& context_;
+    std::string stateName_;
     std::vector<std::shared_ptr<ButtonBase>> button_;
 };
